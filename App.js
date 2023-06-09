05405be0a1156d3e38203d2e4673a1338bb6e962
ij2356 <ij2356@m-sync.co.kr>
2023-06-09 15:48:27 +0900

    first

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+
+const WIDTH = Dimensions.get("window").width;
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const {granted} = await Location.requestForegroundPermissionsAsync(); 
+    if (!granted) {
+      setOk(false);
+    }
+    const location = await Location.getCurrentPositionAsync({ accuracy: 5 })
+    console.log(location)
+  }
+
+  useEffect(() => {
+    ask();
+  }, []);
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView horizontal pagingEnabled style={styles.wheater}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: 500,
+  },
+  wheater: {
+    flex:3,
+  },
+  day: {
+    width: WIDTH,
+    marginTop:20,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  temp: {
+    fontSize: 178,
+  },
+  description: {
+    fontSize: 80,
   },
 });
