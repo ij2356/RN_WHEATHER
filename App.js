f04d0cb1741536d56791bd7ae2780b1edb39501e
ij2356 <ij2356@m-sync.co.kr>
2023-06-16 18:28:15 +0900

    날씨 api fetch 퇴근전 커밋

--- App.js
+++ App.js
@@ -1,54 +1,85 @@
 import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Dimensions, ActivityIndicator } from "react-native";
 import React, { useEffect, useState } from "react";
 
 const WIDTH = Dimensions.get("window").width;
 
 export default function App() {
-  const [location, setLocation] = useState();
+  const [days, setDays] = useState([]);
+  const [city, setCity] = useState(["Loading", ""]);
   const [ok, setOk] = useState(true);
 
-  const ask = async () => {
+  const API_KEY = "5b6469430a7ba339ad8e4e12d62ab57a";
+
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
     }
-<<<<<<< HEAD
-    const { coords : {latitude, longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 })
-    const location = await Location.reverseGeocodeAsync({ latitude, longitude });
-    
-  }
-=======
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    const location = await Location.reverseGeocodeAsync({ latitude, longitude });
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMap: false });
+
+    const str = [location[0].city, location[0].district];
+    setCity(str);
+
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    const json = await res.json();
+    setDays([
+      json.list[0],
+      ...json.list.filter((weather, index) => {
+        if (index !== 0 && weather.dt_txt.includes("00:00:00")) {
+          return weather;
+        }
+      }),
+    ]);
+    console.log(days[0]); 
   };
->>>>>>> dadf8be508257b4596b73d19402d82aa0484e12a
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
 
+  const DatetypeChange = (date) => {
+    var date = new Date(date);
+    var year = date.getFullYear();
+    var month = String(date.getMonth() + 1).padStart(2, "0");
+    var day = String(date.getDate()).padStart(2, "0");
+    var formattedDate = year + "." + month + "." + day;
+
+    return formattedDate;
+  };
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>SEUOL</Text>
+        <Text style={styles.cityName}>
+          {city[0]}
+          {"\n"}
+          {city[1]}
+        </Text>
       </View>
-      <ScrollView horizontal pagingEnabled style={styles.wheater}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+      <ScrollView pagingEnabled horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.weather}>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" style={{ marginTop: 10 }} size="large" />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.tinyText1}>
+                {DatetypeChange(day.dt_txt)}
+              </Text>
+              <Text style={styles.temp}>
+                {parseFloat(day.main.temp).toFixed(1)}
+                <Text style={styles.tinyText3}>°C</Text>
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText2}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
       <StatusBar style="auto" />
     </View>
@@ -56,30 +87,41 @@ export default function App() {
 }
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 80,
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: "#fff",
   },
   city: {
-    flex: 0.5,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   cityName: {
-    fontSize: 68,
+    fontSize: 48,
     fontWeight: 500,
+    width: WIDTH,
+    height: "auto",
+    textAlign: "center",
+    marginBottom: 20,
+    lineHeight: 50,
   },
-  wheater: {
-    flex: 3,
-  },
+  wheater: {},
   day: {
     width: WIDTH,
     marginTop: 20,
     alignItems: "center",
   },
   temp: {
-    fontSize: 178,
+    fontSize: 120,
   },
   description: {
     fontSize: 80,
+  },
+  tinyText1: {
+    fontSize: 24,
+  },
+  tinyText3: {
+    fontSize: 48,
+    fontWeight: 700,
   },
 });
