499ae009ee56e111eb4fb7ad4f0fb50936863496
ksm <ij2356@naver.com>
2023-06-22 01:01:16 +0900

    앱에서 인풋 사용

--- App.js
+++ App.js
@@ -1,139 +1,65 @@
-import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View, Dimensions, ActivityIndicator } from "react-native";
-import React, { useEffect, useState } from "react";
-import { Fontisto } from "@expo/vector-icons";
+import React, { useState} from "react";
+import { StyleSheet, Text,TextInput, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from "react-native";
+import { theme } from "./colors";
 
-const WIDTH = Dimensions.get("window").width;
-
-const icons = {
-  Clouds: "cloudy",
-  Clear: "day-sunny",
-  Atmosphere: "cloudy-gusts",
-  Snow: "snow",
-  Rain: "rains",
-  Drizzle: "rain",
-  Thunderstorm: "lightning",
-};
 export default function App() {
-  const [days, setDays] = useState([]);
-  const [city, setCity] = useState(["Loading", ""]);
-  const [ok, setOk] = useState(true);
-
-  const API_KEY = "5b6469430a7ba339ad8e4e12d62ab57a";
-
-  const getWeather = async () => {
-    const { granted } = await Location.requestForegroundPermissionsAsync();
-    if (!granted) {
-      setOk(false);
-    }
-    const {
-      coords: { latitude, longitude },
-    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMap: false });
-
-    const str = [location[0].city, location[0].district];
-    setCity(str);
-
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    const json = await res.json();
-    setDays([
-      json.list[0],
-      ...json.list.filter((weather, index) => {
-        if (index !== 0 && weather.dt_txt.includes("00:00:00")) {
-          return weather;
-        }
-      }),
-    ]);
-    console.log(days[0]); 
-  };
-
-  useEffect(() => {
-    getWeather();
-  }, []);
-
-  const DatetypeChange = (date) => {
-    var date = new Date(date);
-    var year = date.getFullYear();
-    var month = String(date.getMonth() + 1).padStart(2, "0");
-    var day = String(date.getDate()).padStart(2, "0");
-    var formattedDate = year + "." + month + "." + day;
-
-    return formattedDate;
-  };
+  const [work, setWork] = useState(true);
+  const [text, setText] = useState(""); 
+  
+  const changeText = (event) => {
+    setText(event)
+  }
+  const working = () => {
+    setWork(true);
+  }
+  const travel = () => {
+    setWork(false);
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.city}>
-        <Text style={styles.cityName}>
-          {city[0]}
-          {"\n"}
-          {city[1]}
-        </Text>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={working}>
+          <Text style={{ ...styles.btnText, color: work ? "#fff" : theme.grey }}>Work</Text>
+        </TouchableOpacity >
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !work ? "#fff" : theme.grey }}>Travel</Text>
+        </TouchableOpacity>
       </View>
-      <ScrollView pagingEnabled horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.weather}>
-        {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator color="white" style={{ marginTop: 10 }} size="large" />
-          </View>
-        ) : (
-          days.map((day, index) => (
-            <View key={index} style={styles.day}>
-              <Text style={styles.tinyText1}>{DatetypeChange(day.dt_txt)}</Text>
-              <View style={{flexDirection:"row",alignItems:"center",justifyContent:"space-between",width:WIDTH,paddingRight:20,paddingLeft:20}}>
-                <Text style={styles.temp}>
-                  {parseFloat(day.main.temp).toFixed(1)}
-                  <Text style={styles.tinyText3}>°C</Text>
-                </Text>
-                <Text style={styles.description}>
-                  <Fontisto name={icons[day.weather[0].main]} size={68} color="black" />
-                </Text>
-              </View>
-              <Text style={styles.tinyText2}>{day.weather[0].description}</Text>
-            </View>
-          ))
-        )}
-      </ScrollView>
+
+      <TextInput
+        style={styles.input}
+        placeholder={work ? "어떤일을 하실건가요?" : "여행을 떠나실 장소는?"}
+        value={text}
+        onChangeText={changeText}
+      />
+        
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 80,
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
-  city: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
   },
-  cityName: {
-    fontSize: 48,
-    fontWeight: 500,
-    width: WIDTH,
-    height: "auto",
-    textAlign: "center",
-    marginBottom: 20,
-    lineHeight: 50,
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
   },
-  wheater: {},
-  day: {
-    width: WIDTH,
-    marginTop: 20,
-    alignItems: "center",
-  },
-  temp: {
-    fontSize: 80,
-  },
-  description: {
-    fontSize: 80,
-  },
-  tinyText1: {
-    fontSize: 24,
-  },
-  tinyText3: {
-    fontSize: 48,
-    fontWeight: 700,
-  },
+  input: {
+    fontSize: 20,
+    marginTop: 40,
+    paddingHorizontal: 20,
+    paddingVertical: 20, 
+    backgroundColor : "#fff"
+  }
 });
