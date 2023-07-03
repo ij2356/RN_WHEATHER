cba0a14bcf04917debc301f288b499adc77ae601
ksm <ij2356@naver.com>
2023-07-03 23:51:13 +0900

    AsyncStorage 공부

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StyleSheet, Text, TextInput, View, TouchableOpacity, ScrollView, TouchableHighlight, TouchableWithoutFeedback, Pressable } from "react-native";
 import { theme } from "./colors";
 
+
+const STORAGE_KEY = "@toDos";
 export default function App() {
+  useEffect(() => {
+    loadTodo();
+  },[])
   const [work, setWork] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
@@ -16,17 +22,24 @@ export default function App() {
   const travel = () => {
     setWork(false);
   };
+  const saveTodo = async (saveValue) => {
+    await AsyncStorage.setItem(STORAGE_KEY,JSON.stringify(saveValue));
+  }
+  const loadTodo = async () => {
+    await AsyncStorage.getItem(JSON.parse(STORAGE_KEY))
+  }
 
-  const addTodo = () => {
+  const addTodo = async () => {
     if (text === "") {
       alert("빈값입니다.");
       return;
     }
     const newToDos = {
       ...toDos,
-      [Date.now()]: { text, work: working },
+      [Date.now()]: { text, work },
     };
     setToDos(newToDos);
+    saveTodo(newToDos);
     setText("");
   };
 
@@ -43,11 +56,13 @@ export default function App() {
       </View>
       <TextInput onSubmitEditing={addTodo} returnKeyType="done" style={styles.input} placeholder={work ? "어떤일을 하실건가요?" : "여행을 떠나실 장소는?"} value={text} onChangeText={changeText} />
       <ScrollView>
-        {Object.keys(toDos).map((value) => (
-          <View style={styles.todoInner}>
-            <Text style={styles.todo}>{toDos[value].text}</Text>
-          </View>
-        ))}
+        {Object.keys(toDos).map((value, key) =>
+          toDos[value].work == work ? (
+            <View style={styles.todoInner} key={`a${ key }`}>
+              <Text style={styles.todo}>{toDos[value].text}</Text>
+            </View>
+          ) : null
+        )}
       </ScrollView>
     </View>
   );
@@ -78,7 +93,7 @@ const styles = StyleSheet.create({
   },
   todo: {
     fontSize: 20,
-    bacckground: theme.bg,
+    backgroundColor: theme.bg,
     color: "#fff",
   },
   todoInner: {
